db5041b19f48892e1483a87d8eb17168a9377b5f
SOUS021 <joao.vale@arvato.com>
2020-10-20 23:13:59 +0300

    initial commit

---
+++ src/main/java/com/fortumo/web/SumUpServlet.java
@@ -0,0 +1,58 @@
+package com.fortumo.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet(urlPatterns = "/")
+public class SumUpServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -2245494110623955028L;
+
+    private static final Logger log = Logger.getLogger(SumUpServlet.class.getName());
+    private final AtomicInteger sum = new AtomicInteger(0);
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String value = getValueFromRequest(request);
+        processRequest(response, value);
+    }
+
+    private void processRequest(HttpServletResponse response, String value) {
+        try {
+            synchronized (this) {
+                while (!isEndToken(value)) {
+                    sum.addAndGet(Integer.parseInt(value));
+                    this.wait();
+                }
+                this.notifyAll();
+                response.getOutputStream().print(String.valueOf(sum));
+            }
+        } catch (InterruptedException exception) {
+            Thread.currentThread().interrupt();
+            log.log(Level.SEVERE, "Error processing request: {}", exception.getMessage());
+        } catch (IOException exception) {
+            log.log(Level.SEVERE, "Error writing the response: {}", exception.getMessage());
+        }
+    }
+
+    private static String getValueFromRequest(HttpServletRequest request) {
+        String value = "0";
+        try {
+            value = request.getReader().readLine();
+            log.log(Level.INFO, "Received value:{}", value);
+        } catch (IOException exception) {
+            log.log(Level.SEVERE, "Error parsing received value: {}", exception.getMessage());
+        }
+        return value;
+    }
+
+    private static boolean isEndToken(String value) {
+        return "end".equals(value);
+    }
+}
